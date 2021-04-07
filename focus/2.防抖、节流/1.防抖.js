8719c00fe2bab92e748e38c8722ec9c206d40fcb
lishu <1282150353@qq.com>
2021-04-07 20:59:12 +0800

    debouce

---
+++ focus/2.防抖、节流/1.防抖.js
@@ -0,0 +1,16 @@
+// function debounce(fn, wait) {
+//   let timeout;
+//   return function () {
+//     const context = this;
+//     const args = arguments;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(function () {
+//       fn.apply(context, args);
+//     }, wait);
+//   }
+// }
+
+function debounce(fn, wait, immediate) {
+  let timeout;
+  if(timeout) {clearTimeout}
+}
