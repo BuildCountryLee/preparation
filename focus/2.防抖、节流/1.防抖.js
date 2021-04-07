d44e2604d74215bb89b178952b4b97ebc223330b
1282150353@qq.com <1282150353@qq.com>
2021-04-08 01:37:58 +0800

    debounce

--- focus/2.防抖、节流/1.防抖.js
+++ focus/2.防抖、节流/1.防抖.js
@@ -10,7 +10,34 @@
 //   }
 // }
 
-function debounce(fn, wait, immediate) {
+function debounce(func, wait, immediate) {
   let timeout;
-  if(timeout) {clearTimeout}
+  return function () {
+    const context = this;
+    const args = arguments;
+    if(immediate) {
+      if(timeout) clearTimeout(timeout);
+      const callNow = !timeout;
+      
+
+    }else {
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        func.apply(context, args);
+      }, wait);
+    }
+  }
 }
+
+function debounce(fn, wait) {
+  let timeout;
+  return function() {
+    const context = this;
+    const args = arguments;
+    timeout = setTimeout(function(){
+
+    }, wait);
+  }
+}
+
+const fn = debounce(function() {}, 1000, true);
