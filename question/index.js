a7b0ac4aeb64948460aa624757b6c7b89d7d836e
lishu <1282150353@qq.com>
2021-04-09 17:31:32 +0800

    dev

---
+++ question/index.js
@@ -0,0 +1,26 @@
+var intervals = [[2,3],[5,5],[2,2],[3,4],[3,4]];
+
+function merge(intervals) {
+  var flag = false;
+  var _intervals = intervals.map(interval => interval.map(item => item));
+
+  for (var i = 0; i < intervals.length; i++) {
+    var front = _intervals[i], last = _intervals[i + 1];
+
+    if (last && !((front[1] < last[0]) || (front[0] > last[1]))) {
+      _intervals[i] = [Math.min(front[0], last[0]), Math.max(front[1], last[1])];
+      _intervals.splice(i + 1, 1);
+      flag = true;
+      i--;
+    }
+  }
+
+  if (flag) {
+    return merge(_intervals);
+  }
+
+  console.log(_intervals);
+  return _intervals;
+}
+
+merge(intervals);
